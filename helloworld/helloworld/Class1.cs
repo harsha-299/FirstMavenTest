46cfbddb44ee3bd7345f83c00929e8a833879113
harsha-299 <67918650+harsha-299@users.noreply.github.com>
2020-07-08 18:15:44 +0530

    Add files via upload

---
+++ helloworld/helloworld/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helloworld
+{
+    class Class1
+    {
+        public int length;
+       
+        public int Length
+        {
+            get
+            {
+                return length;
+
+            }
+            set
+            {
+                length = value;
+            }
+        }
+            public void display()
+        {
+            Console.WriteLine("length is{0}", length);
+        }
+        }
+        }
+           
+
+        
+
+    }
+}
---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Class1 sa = new Class1();
+            sa.Length = 4;
+
+        }
+        
+
+  
+    
+    
+    
+    }
+}
